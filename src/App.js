9d555d132aa0775a6417feaa072d1be68396ac6b
Kevin-Wan <xiankai.wan@gmail.com>
2017-11-29 17:29:14 +0800

    Feature：新增addContact功能

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import CreateContacts from './CreateContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
+    screen: 'list', // list or create
     contacts:[]
   }
-  ComponentDidMount(){
+  componentDidMount(){
     ContactsAPI.getAll().then((contacts)=> {
       this.setState({contacts})
     })  
@@ -15,13 +17,24 @@ class App extends Component {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
+    ContactsAPI.remove(contact)
   }
   render() {
     return (
-      <div className="App">
-        <ListContacts 
-        onDeleteContact={this.removeContact} 
-          contacts={this.state.contacts}/>
+        <div className="App">
+        {this.state.screen === 'list' && (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+            addContact={()=>{
+              this.setState({ screen: 'create' })
+            }
+          }
+            />
+      )}
+      {this.state.screen === 'create' && (
+            <CreateContacts />
+      )}
       </div>
     );
   }
